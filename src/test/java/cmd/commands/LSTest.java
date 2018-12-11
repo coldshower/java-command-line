004a3979b94c44c4967fbb183fb0623aa4819abc
coldshower <john@johnchen.co>
2018-12-11 17:19:32 -0500

    tests in progress

---
+++ src/test/java/cmd/commands/LSTest.java
@@ -0,0 +1,12 @@
+package cmd.commands;
+
+import org.junit.Test;
+
+public class LSTest {
+    private final LS ls = new LS();
+
+    @Test
+    public void listsAllContentsAtPath() {
+
+    }
+}
--- src/test/java/cmd/commands/TOUCHTest.java
+++ src/test/java/cmd/commands/TOUCHTest.java
@@ -1,5 +1,34 @@
+package cmd.commands;
+
+import com.sun.jdi.connect.Connector;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Matchers;
+import org.mockito.Spy;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+
+import static org.mockito.Mockito.*;
+
 public class TOUCHTest {
+    private final TOUCH touch = new TOUCH();
+    private final String[] args = new String[] {"touch", "./newDir"};
+
     @Test
+    public void printsErrorIfFailsToCreateFile() {
 
+        doNothing()
+        .doThrow(new IOException("IOException error"))
+        .when(path)
+        .createNewFile();
 
+        PrintStream systemOut = spy(System.out);
+        touch.run(args);
+//        verify(systemOut.println("IOException error"));
+//        ArgumentCaptor<String> captur = ArgumentCaptor.forClass(String);
+        verify(systemOut).println("IOException");
+
+    }
 }
